9fb597aadb23b139addbf29d36a9564dbe708f77
손은하 <ehson@plateer.com>
2024-07-15 21:44:31 +0900

    fix related contents style
    - fix style align center when only 1 contents

--- components/product/product-detail/product-detail-related-contents.tsx
+++ components/product/product-detail/product-detail-related-contents.tsx
@@ -10,24 +10,28 @@ interface ProductDetailRelatedContentsProps {
 
 export default function ProductDetailRelatedContents({ relatedContents }: ProductDetailRelatedContentsProps) {
   return (
-    <div className="mb-16">
-      <H as="h2" className="mb-4">
-        함께 보면 좋아요
-      </H>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        {relatedContents &&
-          relatedContents.map((content, index) => (
-            <div key={content.url + index} className="max-w-80 mx-auto p-2">
-              <Link href={content.url}>
-                <Image className="mb-4" src={content.imgPath} alt={content.title} width={320} height={480} />
-              </Link>
-              <h3 className="text-lg mb-1 mx-auto w-fit">{content.title}</h3>
-              <P className="mx-auto w-fit">
-                {content.author} | {content.publisher} | {content.year}
-              </P>
-            </div>
-          ))}
-      </div>
-    </div>
+    <>
+      {relatedContents && (
+        <div className="mb-16">
+          <H as="h2" className="mb-4">
+            함께 보면 좋아요
+          </H>
+          <div className={"grid grid-cols-1 " + (relatedContents.length > 1 ? "md:grid-cols-2" : "md:grid-cols-1")}>
+            {relatedContents &&
+              relatedContents.map((content, index) => (
+                <div key={content.url + index} className="max-w-80 mx-auto p-2">
+                  <Link href={content.url}>
+                    <Image className="mb-4" src={content.imgPath} alt={content.title} width={320} height={480} />
+                  </Link>
+                  <h3 className="text-lg mb-1 mx-auto w-fit">{content.title}</h3>
+                  <P className="mx-auto w-fit">
+                    {content.author} | {content.publisher} | {content.year}
+                  </P>
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
